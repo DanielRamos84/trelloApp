17955559d686e4314398f99478ea1e33f4197958
Daniel Ramos <1979ramosdan@gmail.com>
2022-03-02 05:11:56 -0800

    Refactored test so each it block can run idenpendently. Decoupled test

--- cypress/integration/owntests/trellotest.spec.js
+++ cypress/integration/owntests/trellotest.spec.js
@@ -1,51 +1,78 @@
 ///<reference types="Cypress"/>
 describe('Trello flow test',()=>{
-    it('Create our first board',()=>{
+    beforeEach(() => {
+        cy.request('DELETE', "/api/boards");
+        
         cy.visit('/');
+   });
+
+    it('Create our first board',()=>{
         cy.addFirstBoard('Board #1');
     });
 
-    after(()=>{
-        cy.request('DELETE', "/api/boards");
-    });
-
     it('Create list, check text', ()=>{
+        cy.addFirstBoard('Board #1');
         cy.addList('List Item #1')
     });
 
     it('Create task, check task\'s text',()=>{
+        cy.addFirstBoard('Board #1');
+        cy.addList('List Item #1');
         cy.addTask('Task #1');
         cy.addTask('Task #2');
         cy.checkTaskText();
     });
 
     it('Drag and Drop test',()=>{
+        cy.addFirstBoard('Board #1');
+        cy.addList('List Item #1');
+        cy.addTask('Task #1');
+        cy.addTask('Task #2');
         cy.dragDrop();
-    })
+    });
 
     it('Attach image drag and drop',()=>{
-        cy.intercept('POST', 'localhost:3000/api/upload').as('fileUpload')
+        cy.intercept('POST', 'localhost:3000/api/upload').as('fileUpload');
+
+        cy.addFirstBoard('Board #1');
+
+        cy.addList('List Item #1');
+        
+        cy.addTask('Task #1');
+       
         cy.attachFileDragDrop();
+        
         cy.wait('@fileUpload')
-    })
+    });
 
     it('Show / Hide star on board', ()=>{
+        cy.addFirstBoard('Board #1');
+
         cy.contains('.Nav_button', 'My Boards')
             .dblclick({force:true});
 
         cy.showStar();
+
         cy.get('[data-cy="star"]').should('be.visible')
 
         cy.hideStar();
+
         cy.get('[data-cy="star"]').should('not.be.visible')
 
         cy.mouseOverStar();
         cy.get('[data-cy="star"]').should('be.visible')
         
         cy.mouseOutStar();
+
         cy.get('[data-cy="star"]').should('not.be.visible')
     });
+
     it('Use Cypress Real Event plugin for hover test',()=>{
+        cy.addFirstBoard('Board #1');
+
+        cy.contains('div', 'My Boards')
+        .click();
+
         cy.get('[data-cy="board-item"]').realHover();
-    })
+    });
 });
