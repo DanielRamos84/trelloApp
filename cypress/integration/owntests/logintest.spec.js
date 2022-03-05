2d66f8ed6a5696d959082f3c2cb08ebdb4c5af31
Daniel Ramos <1979ramosdan@gmail.com>
2022-03-05 08:36:38 -0800

    Use of dual command contains to fix detached DOM element error.

--- cypress/integration/owntests/logintest.spec.js
+++ cypress/integration/owntests/logintest.spec.js
@@ -1,35 +1,28 @@
 ///<reference types= "cypress"/>
 
 describe('Suite of tests login', () => {
-    before(() => {
+    beforeEach(() => {
         cy.clearCookie('trello_token');
-    })
-
-    it('Login via UI', () => {
         cy.visit('/');
-
+    });
+    
+    it('Login via UI', () => {
         cy.contains('[data-cy="login-menu"]', 'Log in')
-            .should('be.visible')
             .click();
         
         cy.get('[data-cy="login-email"')
-            .should('be.visible')
             .type(`${Cypress.env('email')}`);
         
         cy.get('[data-cy="login-password"]')
-            .should('be.visible')
             .type(`${Cypress.env('password')}`, {log:false});
         
         cy.get('[data-cy="login"')
-            .should('be.visible')
             .click();
 
-        cy.get('[data-cy="logged-user"]')
-            .should('be.visible')
-            .click();
+            cy.contains('[data-cy="logged-user"]', 'some@email.com')
+                .click();
 
         cy.contains('Log out')
-            .should('be.visible')
-            .click();         
+            .click();
     });
 });
