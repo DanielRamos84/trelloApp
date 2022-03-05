a4f8c6f3050d94a4115edaf10e0e42d07a057412
Daniel Ramos <1979ramosdan@gmail.com>
2022-03-05 07:33:36 -0800

    Initialize assertion branch

--- cypress/integration/owntests/logintest.spec.js
+++ cypress/integration/owntests/logintest.spec.js
@@ -1,25 +1,35 @@
-///<reference types= "Cypress"/>
-describe('Login test suite using Cookies',()=>{
-    before('Visit homepage log into the application',()=>{
-        cy.visit('http://localhost:3000/')
-        cy.login();
-        cy.setCookie('trello_token', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InNvbWVAZW1haWwuY29tIiwiaWF0IjoxNjI0Mjk0NDM5LCJleHAiOjE2MjQyOTgwMzksInN1YiI6IjEifQ.qffhJood2ZwmXijWMfwZZOdcQJ_gFagX64VNQx1CFv8')
-        cy.get('[data-cy="logged-user"]').should('contain', 'some@email.com')
-    });
-    
-    after(()=>{
-        cy.getCookies().should('have.length', 2)
-        cy.clearCookie('trello_token');
-        cy.getCookie('trello_token').should('be.null');
-    });
-    
-    it('Visit second board without having to log back in',()=>{
-        cy.visit('http://localhost:3000/board/29454093432');
-        cy.get('[data-cy="board-title"]').should('have.value','Second one')    
-    });
+///<reference types= "cypress"/>
 
-    it('Visit third board without having to log back in',()=>{
-        cy.visit('http://localhost:3000/board/88468223975')
-        cy.get('[data-cy="board-title"]').should('have.value','Third One')
+describe('Suite of tests login', () => {
+    before(() => {
+        cy.clearCookie('trello_token');
+    })
+
+    it('Login via UI', () => {
+        cy.visit('/');
+
+        cy.contains('[data-cy="login-menu"]', 'Log in')
+            .should('be.visible')
+            .click();
+        
+        cy.get('[data-cy="login-email"')
+            .should('be.visible')
+            .type(`${Cypress.env('email')}`);
+        
+        cy.get('[data-cy="login-password"]')
+            .should('be.visible')
+            .type(`${Cypress.env('password')}`, {log:false});
+        
+        cy.get('[data-cy="login"')
+            .should('be.visible')
+            .click();
+
+        cy.get('[data-cy="logged-user"]')
+            .should('be.visible')
+            .click();
+
+        cy.contains('Log out')
+            .should('be.visible')
+            .click();         
     });
 });
